ab3e51528ad4eae7f1d17efbd61e71ac2e5739a2
Monoaratasnim <hossainneeha8@gmail.com>
2026-05-04 23:12:02 +0600

    Updated Some Signup code

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,37 +1,3 @@
-// import { betterAuth } from "better-auth";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-// import { MongoClient } from "mongodb";
-
-// const uri = process.env.AUTH_DB_URI;
-
-// if (!uri) {
-//   throw new Error("AUTH_DB_URI is missing");
-// }
-
-
-// const client = new MongoClient(uri);
-
-
-// let clientPromise = client.connect();
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(async () => {
-//     const connectedClient = await clientPromise;
-//     return connectedClient.db("skillSphere");
-//   }),
-
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-
-//   socialProviders: {
-//     google: {
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     },
-//   },
-// });
-
 
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -55,6 +21,7 @@ export const auth = betterAuth({
 
   emailAndPassword: {
     enabled: true,
+     autoSignIn: false,
   },
 
   socialProviders: {
